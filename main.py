7fa77fef3272d4c2e152211de9fb19493e9b9008
Дамир Ханов <46625865+DamirHanov@users.noreply.github.com>
2021-03-29 20:25:11 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,3 @@
-# import gspread
-# from oauth2client.service_account import ServiceAccountCredentials
-# import json
-#
-#
-# scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/spreadsheets',
-#          'https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/drive']
-#
-#
-# with open('TRRP1-20c5117d16b6.json', 'r') as file:
-#     t = json.loads(file.read())
-#
-# creds = ServiceAccountCredentials.from_json_keyfile_dict(t, scope)
-#
-# client = gspread.authorize(creds)
-#
-# sheet = client.open('ТРРП1').sheet1
-#
-# data = sheet.cell(1, 2).value
-#
-# print(data)
 import os
 import hashlib
 from Crypto.Cipher import AES
@@ -150,19 +129,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-# TRRP1-20c5117d16b6.json
-# {
-#   "type": "service_account",
-#   "project_id": "trrp1-309012",
-#   "private_key_id": "20c5117d16b6b3f90afd53931b3dd152c2dc459d",
-#   "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDHUj8H2fjTVVKz\n+L/NlRKf85KKNe3Su3VDs6h5qmPm6IempxPvw7DsW/HJankJmvOVIUkOLcxRSER7\nVgfRYV3PmKK1YvMVb+hhnuBypXlp5EF6UTNnhrgaNWgjaGHXyR9AAn6B+4Kxv88e\ntfnJI0Om8YUecbS3F7imVIFtBZrKl4edjpggPczbeQJBCwfBQnNwDqaelhy8EtOi\nZQtS66Fzxc3j1Z71BrLWNRBC/oTW0t2U87Ev7p0+md8Y7i58b6AGmXdN8BO2TRoO\n49k0nZ3jBtrO+p+GQ588lfTrZ1n8jk+ZPekC0DiidB5Ckelmb5rABbq4JA+na0Qc\neaN8qLaFAgMBAAECggEAXp454pnEB1pVEK8QKeDOvxRqp2ZzV5p6T6Gs49iRZQ9U\nObXNfn94cLMy44jCHV+rqsrquare92TlcWEFBA2C8WXFM3Ld7vq5JwI7DOFlcfGT\nbaO3Ubk/kt1waoKGE9/bEFE+yahhwcxKX4tGIIe3eTvkk++pOFMv6fFqoRzMfse6\nES6I32buqnOldVWWHsSI+BF/AXaud5I0+vFObdDF5HJ+1WrMZIxxqB+VdSJ/bpx2\nkNX99ALd3GDuFYMeBATecIVkrEiTDwP7uEF5ss1mNdS2w4r2v81op+kikovCE5fx\n2JKMwpYOXo73dbMLWIgBTImZ8qqVoc8eMlZaPsC3fwKBgQDwIMk4NiZUlLqqmFCN\n51vmOzjDGQc055UWP2eV/kbCCUuorwxQjgTDvGn2rNkczxdPDTfTkGBBby+V+71e\nnQ4a8if+J9c5fJl4FVZFMADdWyDdK7lOuKzpciQlkzh56eJ3j7MxpUSFM/DaYwKa\nzX5jUebJaH+KOBx0AZhQyRjpZwKBgQDUfvecUnM4OpyfLvN25fRUIpnVuPMBLCaC\nvDMT6vs4Hq7lvijr7R/5s/LOmbJ6rgLyomgk6r9j/d47nYIEW8JaD5oeEl3dL99D\nfZPMq6m7mIiNf6CA0uL/wZmz9jzmcGeTICfALFO5rjNMU7Ir+oDaxSk+v9/EAcgH\nw7eHVN6xMwKBgBN7RXf3BLMWAfL3OonYvF74bQl/DVOgejr81+WWZJ03bdj4orIi\nY4aR0bL+oPqyXny+YMocS4Ljh2POsbVsXomXHeGDz/VNA5J3gLFKTLeovgjTlEwr\ntXOXHBGkWQ+jBwmWMf6UHvQDm8XMBJkUlO1v5p5uiPJozP4oZvge6JN/AoGAWB/r\numWrKPeNuqpzB60oHbhXyf2wsZzIv9Ei8bCyzLxU1ix9thIZ/6l0GeA914jQ6iUW\nQbEk/GftQRX6NKqFOGpeBPii+rb+xXOP1wKeGpGQl/YE70gGIgD9KFgHO54EJkzg\nj18Svd+ToKTZMEnsJE4946sZNqVyel3df/9fd2cCgYBkPZpt6ZmyOvPK/7cNT2SU\nTVEwKlyWfJSB+5Kun/vyE9ZPe+8MuVSjSRvZat8GAwb8Nnpt5UDPRXP6DSriSDP5\nPWpSN9ttndCkEvT/ia6rBvQclGzcitHLQ1XW4zLZKAN92rnvYP6zznVdG6EKIwE3\ng3WckSsf/sG9rhmfl1qxww==\n-----END PRIVATE KEY-----\n",
-#   "client_email": "damir-328@trrp1-309012.iam.gserviceaccount.com",
-#   "client_id": "109717739470725967932",
-#   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-#   "token_uri": "https://oauth2.googleapis.com/token",
-#   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-#   "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/damir-328%40trrp1-309012.iam.gserviceaccount.com"
-# }
-#
